eb3a80b337f1e38b5b7712418f61265078fef640
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-09-09 16:09:57 -0700

    stretch goal: added components to set home and away scores manually

--- src/App.js
+++ src/App.js
@@ -15,23 +15,85 @@ function ScoreContainer(props) {
   );
 }
 
+function TeamSetter(props) {
+  const [team, setTeamName] = useState("");
+  const [score, setScore] = useState(0);
+
+  return (
+    <div>
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          props.setTeam(team);
+        }}
+      >
+        <label>
+          <h3>{props.homeOrAway} Team</h3>
+          <input
+            type="text"
+            value={team}
+            onChange={event => {
+              event.preventDefault();
+              setTeamName(event.target.value);
+            }}
+          ></input>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          props.setTeamScore(score);
+        }}
+      >
+        <label>
+          <h4>Score:</h4>
+          <input
+            type="number"
+            value={score}
+            onChange={event => {
+              event.preventDefault();
+              setScore(event.target.value);
+            }}
+          ></input>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [lionsScore, setLionsScore] = useState(0);
-  const [tigersScore, setTigersScore] = useState(0);
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
   const [time, setTime] = useState("00:00");
   const [down, setDown] = useState(1);
   const [toGo, setToGo] = useState(10);
   const [ballOn, setBallOn] = useState(0);
   const [quarter, setQuarter] = useState(1);
+  const [homeTeam, setHomeTeam] = useState("");
+  const [awayTeam, setAwayTeam] = useState("");
 
   return (
     <div className="container">
+      <section class="setters">
+        <TeamSetter
+          homeOrAway="Home"
+          setTeam={setHomeTeam}
+          setTeamScore={setHomeScore}
+        />
+        <TeamSetter
+          homeOrAway="Away"
+          setTeam={setAwayTeam}
+          setTeamScore={setAwayScore}
+        />
+      </section>
       <section className="scoreboard">
         <div className="topRow">
-          <ScoreContainer team="Lions" homeOrAway="home" score={lionsScore} />
+          <ScoreContainer team={homeTeam} homeOrAway="home" score={homeScore} />
           <div className="timer">{time}</div>
-          <ScoreContainer team="Tigers" homeOrAway="away" score={tigersScore} />
+          <ScoreContainer team={awayTeam} homeOrAway="away" score={awayScore} />
         </div>
         <BottomRow down={down} toGo={toGo} ballOn={ballOn} quarter={quarter} />
       </section>
@@ -40,13 +102,13 @@ function App() {
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button
             className="homeButtons__touchdown"
-            onClick={() => setLionsScore(lionsScore + 7)}
+            onClick={() => setHomeScore(homeScore + 7)}
           >
             Home Touchdown
           </button>
           <button
             className="homeButtons__fieldGoal"
-            onClick={() => setLionsScore(lionsScore + 3)}
+            onClick={() => setHomeScore(homeScore + 3)}
           >
             Home Field Goal
           </button>
@@ -54,13 +116,13 @@ function App() {
         <div className="awayButtons">
           <button
             className="awayButtons__touchdown"
-            onClick={() => setTigersScore(tigersScore + 7)}
+            onClick={() => setAwayScore(awayScore + 7)}
           >
             Away Touchdown
           </button>
           <button
             className="awayButtons__fieldGoal"
-            onClick={() => setTigersScore(tigersScore + 3)}
+            onClick={() => setAwayScore(awayScore + 3)}
           >
             Away Field Goal
           </button>
